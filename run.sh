cc6e4befd14d57c76b31d65b5b80eeabcd638795
coderliux <ecliujie@gmail.com>
2015-12-09 12:49:31 +0800

    [JL] -#471 -add setup for cubes service

--- run.sh
+++
@@ -1,5 +0,0 @@
-virtualenv --python=/usr/bin/python2.7 env
-source env/bin/activate
-pip install -r requirements.txt
-ruby replace_db_credentials.rb
-slicer serve slicer.ini
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from cubes.server import create_server
-
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Set the configuration file name (and possibly whole path) here
-CONFIG_PATH = os.path.join(CURRENT_DIR, "slicer.ini")
-
-application = create_server(CONFIG_PATH)
